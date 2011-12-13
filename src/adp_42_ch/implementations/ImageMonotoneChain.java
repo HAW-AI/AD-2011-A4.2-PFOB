15f4496e9beb717fd13930c29b9319a288628622
panos <panagiotis.filippidis@haw-hamburg.de>
2011-12-13 20:09:57 +0100

    MonotoneChain Klasse bereinigt

--- src/adp_42_ch/implementations/ImageMonotoneChain.java
+++ src/adp_42_ch/implementations/ImageMonotoneChain.java
@@ -4,18 +4,13 @@
  */
 package adp_42_ch.implementations;
 
-import adp_42_ch.implementations.comparators.LexicographicComparatorXfirst;
-import java.util.Arrays;
 import adp_42_ch.interfaces.Point;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
-import java.util.SortedSet;
-import java.util.TreeSet;
-
-import static adp_42_ch.implementations.Images.point;
 
 /**
  *
@@ -33,61 +28,57 @@ public class ImageMonotoneChain extends AbstractImage {
 
     @Override
     protected List<Point> convexHull_(Collection<Point> pointsCollection) {
-        SortedSet<Point> pointSet = new TreeSet<Point>(pointsCollection);
-        List<Point> pointList = new LinkedList<Point>(pointSet);
-        ListIterator<Point> li = pointList.listIterator();
-        Point p;
-        // lower
         List<Point> lowerHull = new ArrayList<Point>();
-        while (li.hasNext()) {
-            p = li.next();
-            while (lowerHull.size() > 1 && T(lowerHull.get(lowerHull.size() - 2), lowerHull.get(lowerHull.size() - 1), p) > 0) {
-                lowerHull.remove(lowerHull.size() - 1);
+        if (pointsCollection.size() > 1) {
+            List<Point> pointsList = new LinkedList<Point>(pointsCollection);
+            Collections.sort(pointsList, new adp_42_ch.implementations.comparators.LexicographicComparatorXfirst());
+            ListIterator<Point> li = pointsList.listIterator();
+            Point p;
+            // lower
+            while (li.hasNext()) {
+                p = li.next();
+                while (lowerHull.size() > 1 && T(lowerHull.get(lowerHull.size() - 2), lowerHull.get(lowerHull.size() - 1), p) >= 0) {
+                    lowerHull.remove(lowerHull.size() - 1);
+                }
+                lowerHull.add(p);
             }
-            lowerHull.add(p);
-        }
-        lowerHull.remove(lowerHull.size() - 1);
+            lowerHull.remove(lowerHull.size() - 1);
 
 
-        // upper
-        List<Point> upperHull = new ArrayList<Point>();
-        while (li.hasPrevious()) {
-            p = li.previous();
-            while (upperHull.size() > 1 && T(upperHull.get(upperHull.size() - 2), upperHull.get(upperHull.size() - 1), p) > 0) {
-                upperHull.remove(upperHull.size() - 1);
+            // upper
+            List<Point> upperHull = new ArrayList<Point>();
+            while (li.hasPrevious()) {
+                p = li.previous();
+                while (upperHull.size() > 1 && T(upperHull.get(upperHull.size() - 2), upperHull.get(upperHull.size() - 1), p) >= 0) {
+                    upperHull.remove(upperHull.size() - 1);
+                }
+                upperHull.add(p);
             }
-            upperHull.add(p);
-        }
-        upperHull.remove(upperHull.size() - 1);
+            upperHull.remove(upperHull.size() - 1);
 
-        lowerHull.addAll(upperHull);
+            lowerHull.addAll(upperHull);
+        } else {
+            lowerHull.addAll(pointsCollection);
+        }
         return lowerHull;
-    }
-
-    @Override
-    protected List<Point> innerPoints_(Collection<Point> points, List<Point> convexHull) {
-        List<Point> innerPoints = new ArrayList<Point>(points);
-        innerPoints.removeAll(convexHull);
-        return innerPoints;
     }
 
     private int T(Point p0, Point p1, Point p2) {
         return (p1.x() - p0.x()) * (p2.y() - p0.y()) - (p2.x() - p0.x()) * (p1.y() - p0.y());
     }
-
-    public static void main(String[] args) {
-        SortedSet<Point> hull = new TreeSet<Point>(new LexicographicComparatorXfirst());
-        hull.addAll(image(point(2, 1), point(3, 2), point(2, 3), point(1, 2), point(2, 2)).convexHull());
-        System.out.println("Konvexe Hülle:" + hull + "\n\n");
-
-        hull.clear();
-
-        hull.addAll(image(point(2, 1), point(3, 2), point(2, 3), point(1, 2), point(2, 2), point(5, 3), point(3, 4), point(2, 4), point(4, 3)).convexHull());
-        System.out.println("Konvexe Hülle:" + hull + "\n\n");
-
-    }
-
-    public static ImageMonotoneChain image(Point... points) {
-        return new ImageMonotoneChain(Arrays.asList(points));
-    }
+//    public static void main(String[] args) {
+//        SortedSet<Point> hull = new TreeSet<Point>(new LexicographicComparatorXfirst());
+//        hull.addAll(image(point(2, 1), point(3, 2), point(2, 3), point(1, 2), point(2, 2)).convexHull());
+//        System.out.println("Konvexe Hülle:" + hull + "\n\n");
+//
+//        hull.clear();
+//
+//        hull.addAll(image(point(2, 1), point(3, 2), point(2, 3), point(1, 2), point(2, 2), point(5, 3), point(3, 4), point(2, 4), point(4, 3)).convexHull());
+//        System.out.println("Konvexe Hülle:" + hull + "\n\n");
+//
+//    }
+//
+////    public static ImageMonotoneChain image(Point... points) {
+////        return new ImageMonotoneChain(Arrays.asList(points));
+////    }
 }
---
+++ src/adp_42_ch/implementations/RandomPointsDebugImpl.java
@@ -0,0 +1,91 @@
+package adp_42_ch.implementations;
+
+import java.util.HashSet;
+
+import java.util.Random;
+import java.util.Set;
+import static adp_42_ch.implementations.Images.*;
+
+import adp_42_ch.interfaces.Point;
+import adp_42_ch.interfaces.RandomPoints;
+
+public class RandomPointsImpl implements RandomPoints {
+
+	private final long seed;
+	
+	private final int maxX;
+	
+	private final int maxY;
+	
+	private final int minX;
+	
+	private final int minY;
+	
+	private final Set<Point> points;
+	
+	private final Random random;
+	
+	private RandomPointsImpl (long seed, int numberOfPoints, int minX, int maxX, int minY, int maxY){
+		this.seed=seed;
+		this.random=new Random(seed);
+		this.points=calculatePoints(numberOfPoints, minX, maxX, minY, maxY);
+		this.maxX=maxX;
+		this.maxY=maxY;
+		this.minX=minX;
+		this.minY=minY;
+	}
+	protected static RandomPoints create(int numberOfPoints, int minX, int maxX, int minY, int maxY){
+		return create(System.currentTimeMillis(), numberOfPoints, minX, maxX, minY, maxY);
+	}
+	
+	static RandomPoints create(long seed, int numberOfPoints, int minX, int maxX, int minY, int maxY){
+		if(numberOfPoints<=0 || minX>maxX || minY>maxY || 
+				maxX==Integer.MAX_VALUE || maxY==Integer.MAX_VALUE || maxX+1-minX<0 || maxY+1-minY<0
+				|| ((long)(maxX+1-minX)*(long)(maxY+1-minY))<numberOfPoints) return NaRP();
+		return new RandomPointsImpl(seed, numberOfPoints, minX, maxX, minY, maxY);
+	}
+	
+	private Set<Point> calculatePoints(int numberOfPoints, int minX, int maxX, int minY, int maxY) {
+		Set<Point> result=new HashSet<Point>();
+//		for(int i=0; i<numberOfPoints; i++){
+//			result.add(point(random.nextInt(maxX-minX+1)+minX, random.nextInt(maxY-minY+1)+minY));
+//		}
+		while(numberOfPoints>result.size()) result.add(point(random.nextInt(maxX-minX+1)+minX, random.nextInt(maxY-minY+1)+minY));
+		return result;
+	}
+
+
+	@Override
+	public long seed() {
+		return seed;
+	}
+
+	@Override
+	public Set<Point> points() {
+		return new HashSet<Point>(points);
+	}
+
+	@Override
+	public int size() {
+		return points.size();
+	}
+
+	@Override
+	public int maxX() {
+		return maxX;
+	}
+
+	@Override
+	public int maxY() {
+		return maxY;
+	}
+	@Override
+	public int minX() {
+		return minX;
+	}
+	@Override
+	public int minY() {
+		return minY;
+	}
+
+}
